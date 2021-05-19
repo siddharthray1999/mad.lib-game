b7dcac242d2dab3314fab00bef3e77a621e8f6bd
siddharth ray <70530835+siddharthray1999@users.noreply.github.com>
2021-05-19 19:48:35 +0530

    Add files via upload

---
+++ mad lib game.py
@@ -0,0 +1,16 @@
+print("welcome to the mad lib game - where you come to fill in thh=e blanks in a story.")
+print("our story today is :'the____ ____ _____ over the lazy dog'.")
+print("lets finish the story...")
+print("\n ")
+
+
+
+adjective = input("enter an adjective (a word that describe a noun):")
+noun = input("enter a noun (a person,place or thing):")
+verb = input("enter a verb(a word that describe an action):")
+print("\n")
+
+
+print("your mad lib")
+print("*************")
+print("The",adjective , noun , verb ,"over the lazy dog.")
